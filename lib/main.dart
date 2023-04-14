c0d7382422a6dd0713cf5ccfb63e8755214d407e
Nabeel Shehzad <39397484+Nabeel-Shehzad@users.noreply.github.com>
2023-04-14 15:41:14 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:facebook/screens/nav_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'config/palette.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Facebook UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold,
+      ),
+      home: NavScreen(),
+    );
+  }
+}
